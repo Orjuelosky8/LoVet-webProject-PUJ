2cf317940b96177c191c568bd9763ca77c4df17d
Daniel <daniel.orju8@gmail.com>
2024-03-07 12:15:40 -0500

    Restriction to Login Added and Sweet Alert implemented

--- demo/src/main/resources/static/js/login.js
+++ demo/src/main/resources/static/js/login.js
@@ -2,3 +2,29 @@ let userName = document.getElementById("userNameLogin");
 let mail = document.getElementById("emailLogin");
 let pass = document.getElementById("passwordLogin");
 
+
+// // or via CommonJS
+// const Swal = require('sweetalert2')
+
+// document.getElementById("msgIncorrectLogin-p").addEventListener("click", (event) => {
+//     alert("hola")
+// })
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const errorMessage = document.getElementById('msgIncorrectLogin-p');
+    if (errorMessage && errorMessage.textContent.trim() !== '') {
+        // // Aquí puedes mostrar el mensaje como prefieras, por ejemplo, usando alert o modificando el DOM para mostrarlo en la página
+        // alert(errorMessage.textContent);
+        // // O mostrar el div y aplicarle estilos
+        // errorMessage.style.display = 'block';
+        // errorMessage.style.color = 'red'; // Agrega más estilos según necesites
+        Swal.fire({
+            title: 'Error!',
+            text: 'Usuario o contraseña incorrectos, intentelo nuevamente.',
+            icon: 'error',
+            confirmButtonText: 'Aceptar'
+        });
+    }
+});
+
