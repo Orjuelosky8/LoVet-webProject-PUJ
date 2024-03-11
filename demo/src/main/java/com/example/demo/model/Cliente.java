1609bd719dc97d665b479ca489da73d4333b502a
Daniel <daniel.orju8@gmail.com>
2024-03-11 13:22:49 -0500

    Delete Clients Prooblem Fixed

--- demo/src/main/java/com/example/demo/model/Cliente.java
+++ demo/src/main/java/com/example/demo/model/Cliente.java
@@ -3,6 +3,7 @@ package com.example.demo.model;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -24,7 +25,7 @@ public class Cliente {
     @GeneratedValue
     private Long id;
     
-    @OneToMany(mappedBy = "cliente")
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Mascota> mascotas = new ArrayList<>();
 
     
